6747fa4d90e32e6feea1377223665c41b20c6fc8
EJostad <eric.jostad@outlook.com>
2021-05-22 20:09:45 -0700

    Added functionality to operator variable to take in '+' for addition and '-' for subtraction.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
 num1 = input('Enter a number: ')
-num2 = input('Enter another number: ')
 operator = input('Enter an operator: ')
-result = float(num1) + float(num2)
+num2 = input('Enter another number: ')
+
+if operator == '+':
+    print(num1 + num2)
+elif operator == '-':
+    print(num1 - num2)
+
+result = float(num1) + operator + float(num2)
 
 print(result)
